6555c1b8b285e25deca8bb30481f818d2e0ad460
NaChOoV <fuenzalida.veas@gmail.com>
2020-09-21 22:32:20 -0300

    feat(forecast.js): add humidity variable

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,7 +20,9 @@ const forecast = (latitude, longitude, callback) => {
         body.current.temperature +
         " degress out. And feelslike " +
         body.current.feelslike +
-        " degress.";
+        " degress. The humidity is " +
+        body.current.humidity +
+        "%.";
 
       callback(undefined, forecast);
     }
